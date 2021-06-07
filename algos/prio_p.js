34005494a3ee11eec5d4197acd5959f9b5f333c9
smit3949 <smit3949@gmail.com>
2021-06-07 11:47:27 +0530

    prio & srtf

--- algos/prio_p.js
+++ algos/prio_p.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-
 router.post('/', (req, res) => {
     var state = req.body;
-    var tuple = [],
-    tuple_temp = [];
+    var tuple = [];
     var n = state.tableData.length;
     for (let i = 0; i < n; i++) {
       var tempPid = state.tableData[i][0].substring(1);
@@ -12,18 +10,13 @@ router.post('/', (req, res) => {
         pid: tempPid,
         bt: parseInt(state.tableData[i][2]),
         art: parseInt(state.tableData[i][1]),
+        prio: parseInt(state.tableData[i][3]),
       });
-      tuple_temp.push({
-        pid: tempPid,
-        bt: parseInt(state.tableData[i][2]),
-        art: parseInt(state.tableData[i][1]),
-      });
-     
     }
     var n = tuple.length;
-    var total_btt = [];
     var artt = [];
-    for (var i = 0; i < n; i++) {
+    var total_btt = [];
+    for (var i = 0; i < tuple.length; i++) {
       total_btt[i] = tuple[i].bt;
       artt[i] = tuple[i].art;
     }
@@ -32,7 +25,6 @@ router.post('/', (req, res) => {
       return a.art - b.art;
     });
     tuple.sort();
-    var n = tuple.length;
     var wt = [];
     var tat = [];
     var total_wt = 0;
@@ -53,36 +45,44 @@ router.post('/', (req, res) => {
           visited[j] = 1;
         }
       }
-      var mn = 0;
+      var mn = 10000;
       var state = -1;
       for (var j = 0; j < n; j++) {
-        if (tuple[j].art <= i && visited[j] === 0) {
-          if (tuple[j].bt > mn) {
-            mn = tuple[j].bt;
+        if (tuple[j].art <= i) {
+          if (tuple[j].prio < mn && visited[j] === 0) {
+            mn = tuple[j].prio;
             state = j;
           }
         }
       }
+
       if (state == -1) {
         final_ans.push("/");
         var smit = [];
         que.push(smit);
       } else {
-        for (var j = 0; j < tuple[state].bt; j++) {
+        for (var j = 0; j < 1; j++) {
           final_ans.push(tuple[state].pid);
         }
-        for (var g = i; g < i + tuple[state].bt; g++) {
+        tuple[state].bt -= 1;
+
+        for (var g = i; g < i + 1; g++) {
           var smit = [];
           for (var y = 0; y < n; y++) {
-            if (tuple[y].art <= g && visited[y] === 0) {
+            if (tuple[y].art <= g || btco[y] == 1) {
               smit.push(tuple[y].pid);
             }
           }
           que.push(smit);
         }
-        i += tuple[state].bt - 1;
-        tuple[state].bt = 0;
+        if (tuple[state].bt <= 0) {
+          visited[state] = 0;
+        }
       }
+    }
+    console.log(que.length);
+    for (var i = 0; i < 50; i++) {
+      console.log(i + " " + final_ans[i]);
     }
     var cmp_time = [];
     for (var i = 0; i < tuple.length; i++) {
@@ -98,12 +98,15 @@ router.post('/', (req, res) => {
     }
     for (var i = 0; i < n; i++) {
       tat[i] = cmp_time[i] - artt[i];
+
       wt[i] = tat[i] - total_btt[i];
     }
     for (var i = 0; i < n; i++) {
       total_wt = total_wt + wt[i];
       total_tat = total_tat + tat[i];
     }
+    console.log(total_wt / n + " " + total_tat / n);
+
     // Changing Pid into string in final answer array
     for (var i = 0; i < final_ans.length; i++) {
       if (final_ans[i] != "/") {
@@ -126,7 +129,6 @@ router.post('/', (req, res) => {
         ganntChartArray: final_ans
     }
     res.send(data);
-
 });
 router.post('/io', (req, res) => {
     var state = req.body;
@@ -141,15 +143,16 @@ router.post('/io', (req, res) => {
         art: parseInt(state.tableData[i][1]),
         io: parseInt(state.tableData[i][3]),
         bt2: parseInt(state.tableData[i][4]),
+        prio: parseInt(state.tableData[i][5]),
       });
     }
     var total_bt = [];
     var artt = [];
     var total_btt = [];
-    var n = tuple.length;
     for (var i = 0; i < tuple.length; i++) {
-      total_bt[i] = tuple[i].bt1 + tuple[i].io + tuple[i].bt2;
-      total_btt[i] = total_bt[i] - tuple[i].io;
+      total_bt[i] = tuple[i].bt1 + tuple[i].bt2;
+
+      total_btt[i] = total_bt[i];
       artt[i] = tuple[i].art;
     }
     var tuple_temp = tuple;
@@ -157,47 +160,53 @@ router.post('/io', (req, res) => {
       return a.art - b.art;
     });
     tuple.sort();
-    var wt = []; //wating time
-    var tat = []; //turnaround time
-    var total_wt = 0; // total wating time
-    var total_tat = 0; // total turnaround time
-    var final_ans = []; // grannt chart
+
+    var n = tuple.length;
+    var wt = [];
+    var tat = [];
+    var total_wt = 0;
+    var total_tat = 0;
+    var final_ans = [];
     var visited = [];
     for (var i = 0; i < tuple.length; i++) {
       visited[i] = 0;
     }
+
     var que = [];
     var btco = [];
     for (var i = 0; i < n; i++) {
       btco[i] = 0;
     }
+
     for (var i = 0; i < 10000; i++) {
       for (var j = 0; j < n; j++) {
         if (total_bt[i] <= 0) {
           visited[i] = 1;
         }
       }
-      var mn = 0;
+      var mn = 10000;
       var state = -1;
       for (var j = 0; j < n; j++) {
         if (tuple[j].art <= i) {
-          if (total_bt[j] > mn) {
+          if (tuple[j].prio < mn) {
             mn = total_bt[j];
             state = j;
           }
         }
       }
+
       if (state == -1) {
         final_ans.push("/");
         var smit = [];
         que.push(smit);
       } else {
         if (btco[state] === 0) {
-          for (var j = 0; j < tuple[state].bt1; j++) {
+          for (var j = 0; j < 1; j++) {
             final_ans.push(tuple[state].pid);
           }
-          tuple[state].art = i + tuple[state].bt1 + tuple[state].io;
-          for (var g = i; g < i + tuple[state].bt1 - 1; g++) {
+          tuple[state].bt1 -= 1;
+          total_bt[state] -= 1;
+          for (var g = i; g < i + 1; g++) {
             var smit = [];
             for (var y = 0; y < n; y++) {
               if (tuple[y].art <= g || btco[y] == 1) {
@@ -206,14 +215,17 @@ router.post('/io', (req, res) => {
             }
             que.push(smit);
           }
-          i += tuple[state].bt1 - 1;
-          btco[state] = 1;
-          total_bt[state] -= tuple[state].bt1 + tuple[state].io;
+          if (tuple[state].bt1 <= 0) {
+            tuple[state].art = i + tuple[state].io + 1;
+            btco[state] = 1;
+          }
         } else {
-          for (var j = 0; j < tuple[state].bt2; j++) {
+          for (var j = 0; j < 1; j++) {
             final_ans.push(tuple[state].pid);
           }
-          for (var g = i; g < i + tuple[state].bt1 - 1; g++) {
+          tuple[state].bt2 -= 1;
+          total_bt[state] -= 1;
+          for (var g = i; g < i + 1; g++) {
             var smit = [];
             for (var y = 0; y < n; y++) {
               if (tuple[y].art <= g || btco[y] == 1) {
@@ -222,12 +234,14 @@ router.post('/io', (req, res) => {
             }
             que.push(smit);
           }
-          i += tuple[state].bt2 - 1;
-          total_bt[state] = 0;
-          tuple[state].art = 10000;
+          if (tuple[state].bt2 <= 0) {
+            tuple[state].art = 10000;
+          }
         }
       }
     }
+    console.log(que.length);
+
     var cmp_time = [];
     for (var i = 0; i < tuple.length; i++) {
       cmp_time[i] = -1;
@@ -240,17 +254,17 @@ router.post('/io', (req, res) => {
         }
       }
     }
-
-    var wt = [];
-
     for (var i = 0; i < n; i++) {
       tat[i] = cmp_time[i] - artt[i];
+
       wt[i] = tat[i] - total_btt[i];
     }
     for (var i = 0; i < n; i++) {
       total_wt = total_wt + wt[i];
       total_tat = total_tat + tat[i];
     }
+    console.log(total_wt / n + " " + total_tat / n);
+
     // Changing Pid into string in final answer array
     for (var i = 0; i < final_ans.length; i++) {
       if (final_ans[i] != "/") {
@@ -258,6 +272,7 @@ router.post('/io', (req, res) => {
         final_ans[i] = "P" + final_ans[i].toString();
       }
     }
+    // Removing '/' from the back of the array
     for (var i = final_ans.length - 1; i >= 0; i--) {
       if (final_ans[i] != "/") break;
       final_ans.pop();
@@ -272,6 +287,5 @@ router.post('/io', (req, res) => {
         ganntChartArray: final_ans
     }
     res.send(data);
-
 });
 module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
 const fcfs = require('./algos/fcfs');
+const ljf = require('./algos/ljf');
+const lrtf = require('./algos/lrtf');
+const srtf = require('./algos/srtf');
+const sjf = require('./algos/sjf');
+const rr = require('./algos/rr');
+const prio_p = require('./algos/prio_p');
+const prio_np = require('./algos/prio_np');
+
 app.use(express.json());
-app.use('/fcfs',fcfs);
+app.use('/fcfs', fcfs);
+app.use('/ljf', ljf);
+app.use('/lrtf', lrtf);
+app.use('/srtf', srtf);
+app.use('/sjf', sjf);
+app.use('/rr', rr);
+app.use('/prio_p', prio_p);
+app.use('/prio_np', prio_np);
 
 app.listen(8000, () => {console.log("server is listning ");})
